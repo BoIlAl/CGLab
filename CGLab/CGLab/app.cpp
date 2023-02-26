98dbd3b764043212c477d951e4f5ad7486171605
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2023-02-26 21:40:07 +0300

    fix crash

--- CGLab/CGLab/app.cpp
+++ CGLab/CGLab/app.cpp
@@ -21,7 +21,12 @@ void App::DeleteAppl(App*& pAppl)
 
 bool App::Resize(UINT newWidth, UINT newHeight)
 {
-	return m_pRenderer->Resize(newWidth, newHeight);;
+	if (newWidth == 0 || newHeight == 0)
+	{
+		return true;
+	}
+
+	return m_pRenderer->Resize(newWidth, newHeight);
 }
 
 void App::Render()
